0923af6349edca1a3fe23b976b81c6e1f0459346
Bakiyama <htt.ikumi@gmail.com>
2021-04-09 10:27:15 +0900

    共通ファイルのheadタグ内を記述

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <title>Laravel Quickstart - Basic</title>
 
+  <!-- Fonts -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+
+  <!-- Style -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 </html>
