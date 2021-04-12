dbca50242c917f5ae9bf3f5dff729f310243a9cf
Bakiyama <htt.ikumi@gmail.com>
2021-04-12 13:08:08 +0900

    タスク一覧表示に削除ボタン追加

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -60,6 +60,16 @@
 
                     <td>
                       <!-- TODO:削除ボタン -->
+                      <td>
+                        <form action="{{ url('task/'.$task->id) }}" method="POST">
+                          {{ csrf_field() }}
+                          {{ method_field('DELETE') }}
+
+                          <button type="submit" class="btn btn-danger">
+                            <i class="fa fa-trash"></i>Delete
+                          </button>
+                        </form>
+                      </td>
                     </td>
                   </tr>
                 @endforeach
